93c36adeeb751e5a6604e40e744248c56796fd17
o0ragman0o <o0ragman0o+git@gmail.com>
2017-08-26 12:49:09 +1000

    v0.2.0 OwnerAbstract and example implementation.

---
+++ README.md
@@ -0,0 +1,60 @@
+## Owned
+
+An expanded `Owned` API for user and inter-contract ownership and safe ownership transfers.
+
+This API encodes a safe pattern of contract ownership transfers in which ownership does not change until the new owner accepts the ownership thus proving the new owner address is capable of calling. This prevents inadvertant loss of contract control by assigning ownership to a wrong address.
+
+### owner
+```
+function owner() public constant returns (address);
+```
+Returns the address of the contract's current owner.
+
+### newOwner
+```
+function newOwner() public constant returns (address);
+```
+Returns the address which is permissioned to accept ownership of the contract
+
+### changeOwner
+```
+function changeOwner(address _newOwner) public returns (bool);
+```
+Permissions an address to accept ownership.
+
+`_newOwner` The address to permission ownership acceptance.
+
+Returns success boolean
+
+### acceptOwnership
+```
+function acceptOwnership() public returns (bool);
+```
+Allows a permissioned address to take ownership of the contract.
+
+### receiveOwnership
+```
+function receiveOwnership(address _kAddr) public returns (bool);
+```
+Allows the contract to accept ownership by calling `acceptOwnership()` of another contract.
+
+`_kAddr` The address of a contract.
+
+returns success boolean.
+
+### ChangeOwner
+```
+event ChangeOwnerTo(address indexed _newOwner);
+```
+Triggered on initiation of ownership transfer.
+
+```
+event ChangedOwner(address indexed _oldOwner, address indexed _newOwner);
+```
+Triggered on change of owner address.
+
+### ReceivedOwnership
+```
+event ReceivedOwnership(address indexed _kAddr);
+```
+Triggered when the contract accepts ownership of another contract.
---
+++ contracts/Owned.sol
@@ -0,0 +1,103 @@
+/******************************************************************************\
+
+file:   Owned.sol
+ver:    0.1.0
+updated:26-Aug-2017
+author: Darryl Morris (o0ragman0o)
+email:  o0ragman0o AT gmail.com
+
+An expanded `Owned` API for user and inter-contract ownership and safe ownership
+transfers.
+
+This software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+See MIT Licence for further details.
+<https://opensource.org/licenses/MIT>.
+
+\******************************************************************************/
+
+pragma solidity ^0.4.13;
+
+contract OwnedAbstract {
+
+//
+// State Variable
+//
+
+    /// @return The address of the contract's owner
+	address owner;
+
+	/// @dev optional
+	/// @return An address which can accept ownership.
+	address newOwner;
+
+	/// @dev Triggered on initiation of change owner address
+    event ChangeOwnerTo(address indexed _newOwner);
+
+    /// @dev Triggered on change of owner address
+    event ChangedOwner(address indexed _oldOwner, address indexed _newOwner);
+
+    /// @dev Triggered when the contract accepts ownership of another contract.
+    event ReceivedOwnership(address indexed _kAddr);
+
+//
+// Modifiers
+//
+
+    modifier onlyOwner {
+		require (msg.sender == owner);
+		_;
+	}
+	
+//
+// Function Abstracts
+//
+
+    /// @notice Initiate a change of owner to `_newOowner`
+    /// @param _newOwner The address to which ownership is to be transfered
+    function changeOwner(address _newOwner) public returns (bool);
+
+    /// @dev optional
+    /// @notice Finalise change of ownership to newOwner
+    function acceptOwnership() public returns (bool);
+
+    /// @dev optional
+    /// @notice This contract will call `_kAddr`.acceptOwnership()
+    /// @param _kAddr A thirdparty contract which this contract can accept
+    /// ownership
+    function receiveOwnership(address _kAddr) public returns (bool);
+}
+
+// Example implementation.
+contract Owned is OwnedAbstract{
+
+	function changeOwner(address _newOwner)
+		public
+		onlyOwner
+		returns (bool)
+	{
+		newOwner == _newOwner;
+	    ChangeOwnerTo(_newOwner);
+		return true;
+	}
+
+	function acceptOwnership()
+		public
+		returns (bool)
+	{
+		require(msg.sender == newOwner);
+	    ChangedOwner(owner, msg.sender);
+		owner = msg.sender;
+		return true;
+	}
+
+	function receiveOwnership(address _kAddr)
+		public
+		returns (bool)
+	{
+		require(OwnedAbstract(_kAddr).acceptOwnership());
+	    ReceivedOwnership(_kAddr);
+	    return true;
+	}
+}
